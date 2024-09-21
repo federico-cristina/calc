190a69e723350a18084a8f851f1d2939ba6e12e8
federico-cristina <federico.cristina@outlook.it>
2024-09-21 15:52:23 +0200

    Add calc_link_libraries.cmake and move CMake properties in calc.cmake

---
+++ cmake/modules/calc_link_libraries.cmake
@@ -0,0 +1,10 @@
+# Usage:
+#
+#   calc_link_libraries(<TARGET>)
+#
+# Links calc libraries to the specified target.
+function(calc_link_libraries TARGET)
+    get_property(_LIBS GLOBAL PROPERTY CALC_TARGETS)
+    target_link_libraries(${TARGET} "${_LIBS}")
+    message(NOTICE "${CALC_LOG_PREFIX}: linked libraries to ${TARGET}")
+endfunction()
