ab8a790b6bc0e79baf646feac66217d0414b12a1
federico-cristina <federico.cristina@outlook.it>
2024-09-21 21:01:09 +0200

    Update error.h

--- include/calc/base/error.h
+++ include/calc/base/error.h
@@ -150,7 +150,7 @@ CALC_INLINE CALC_NORETURN void CALC_CDECL _fail(const char *const format, ...)
 #ifndef failno
 /// @brief Aborts program execution printing an error message based on
 ///        the current value of errno.
-#   define failno() fail("errno %d (%s): %s", errno, errnoname(errno), strerror(errno))
+#   define failno() (fail("errno %d (%s): %s", errno, errnoname(errno), strerror(errno)), NULL)
 #endif // failno
 
 #ifndef unreach
