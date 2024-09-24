6b93686028f969a6fbddb68fa6c2b09739d698f0
federico-cristina <federico.cristina@outlook.it>
2024-09-24 08:48:46 +0200

    Add unit tests support in CMake project

--- cmake/modules/calc.cmake
+++ cmake/modules/calc.cmake
@@ -1,7 +1,8 @@
 define_property(GLOBAL PROPERTY CALC_TARGETS)
 define_property(GLOBAL PROPERTY CALC_LIBRARY)
 
-set(CALC_LIBRARY_PREFIX "calc-lib-")
+set(CALC_LIBRARY_PREFIX   "calc-lib-")
+set(CALC_UNIT_TEST_PREFIX "calc-test-")
 
 set(CMAKE_STATIC_LIBRARY_PREFIX ${CALC_LIBRARY_PREFIX})
 set(CMAKE_SHARED_LIBRARY_PREFIX ${CALC_LIBRARY_PREFIX})
@@ -10,4 +11,5 @@ set(CMAKE_IMPORT_LIBRARY_PREFIX ${CALC_LIBRARY_PREFIX})
 set(CALC_INCLUDE_PREFIX "${CALC_INCLUDE_DIR}/calc")
 
 include(calc_add_library)
+include(calc_add_unit_test)
 include(calc_link_libraries)
--- cmake/modules/calc_link_libraries.cmake
+++ cmake/modules/calc_link_libraries.cmake
@@ -3,6 +3,7 @@
 #   calc_link_libraries(<TARGET>)
 #
 # Links calc libraries to the specified target.
+#
 function(calc_link_libraries TARGET)
     get_property(_LIBS GLOBAL PROPERTY CALC_TARGETS)
     target_link_libraries(${TARGET} "${_LIBS}")
