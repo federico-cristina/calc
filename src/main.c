eaa6078ae0681f442eb157624281863757c94b03
federico-cristina <federico.cristina@outlook.it>
2024-09-25 15:16:21 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,13 +1,4 @@
-#include "calc/base/utf8.h"
-
-int handle(int value)
-{
-    return eprintfn("an error has been occurred: %d.", value), 1;
-}
-
 int main()
 {
-    int i = (utf8_category(0x3042) & UTF8_CATEGORY_LO) == UTF8_CATEGORY_LO;
-    
     return 0;
 }
