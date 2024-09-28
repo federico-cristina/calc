207b086399da4417f8c63efc95c6cabe9b1cc4c3
Federico Cristina <federico.cristina@outlook.it>
2024-09-28 08:46:37 +0200

    Include calc_log to CMake project

--- cmake/modules/calc.cmake
+++ cmake/modules/calc.cmake
@@ -1,6 +1,7 @@
 define_property(GLOBAL PROPERTY CALC_TARGETS)
 define_property(GLOBAL PROPERTY CALC_LIBRARY)
 
+set(CALC_LOG_PREFIX       "calc")
 set(CALC_LIBRARY_PREFIX   "calc-lib-")
 set(CALC_UNIT_TEST_PREFIX "calc-test-")
 
@@ -10,6 +11,7 @@ set(CMAKE_IMPORT_LIBRARY_PREFIX ${CALC_LIBRARY_PREFIX})
 
 set(CALC_INCLUDE_PREFIX "${CALC_INCLUDE_DIR}/calc")
 
+include(calc_log)
 include(calc_add_library)
 include(calc_add_unit_test)
 include(calc_link_libraries)
