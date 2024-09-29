f88054b55fc91da031ff851dcc4e0af3d58b082c
Federico Cristina <federico.cristina@outlook.it>
2024-09-29 11:01:32 +0200

    Update test_source_stream.c

--- units/source/test_source_stream.c
+++ units/source/test_source_stream.c
@@ -2,7 +2,7 @@
 
 int main()
 {
-    CalcSourceStream_t *s = calcOpenSourceStream(CALC_CURRENT_PATH "/docs/examples/f.calc", FALSE);
+    CalcSourceStream_t *s = calcOpenSourceStream(CALC_CURRENT_PATH "/docs/examples/f.calc", FALSE, CALC_DEFAULT_ENCODING);
 
-    
+    calcDeleteSourceStream(s);
 }
