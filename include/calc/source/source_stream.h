30dee269bf89401db84a185b8b20ef008cac06e4
Federico Cristina <federico.cristina@outlook.it>
2024-09-29 11:01:03 +0200

    Update source_stream.h and source_stream.c

--- include/calc/source/source_stream.h
+++ include/calc/source/source_stream.h
@@ -21,6 +21,12 @@
 #include "calc/source/source_buffer.h"
 #include "calc/source/source_location.h"
 
+#ifndef CALC_DEFAULT_ENCODING
+/// @brief This constant macro represents the default encoding used by
+///        new source streams.
+#   define CALC_DEFAULT_ENCODING CALC_SOURCE_ENCODING_UTF_8
+#endif // CALC_DEFAULT_ENCODING
+
 CALC_C_HEADER_BEGIN
 
 /// @brief Source stream data structure.
@@ -33,17 +39,20 @@ typedef struct _CalcSourceStream
     ///        buffer when the cursor reaches the end.
     FILE                *stream;
     /// @brief When it's set to TRUE the source file is treated as an
-    ///        interactive stream and is red line by line.
+    ///        interactive stream and is read line by line.
     bool_t               isStdin;
-    /// @brief When it's set to TRUE means that the source stream uses as
-    ///        source a NUL-terminated string of characters.
-    bool_t               isText;
+    /// @brief When it's set to TRUE means that the source stream has alredy
+    ///        been initialized.
+    bool_t               isInitialized;
     /// @brief When it's set to TRUE means that the source file is alredy
     ///        open.
     bool_t               isOpen;
     /// @brief When it's set to TRUE the path string is released on stream
     ///        deletion.
     bool_t               cleanup;
+    /// @brief The encoding of the source stream (the default encoding is
+    ///        specified by CALC_DEFAULT_ENCODING macro).
+    CalcSourceEncoding_t encoding;
     /// @brief A pointer to the source buffer data structure that stores
     ///        the content of the stream.
     CalcSourceBuffer_t  *buffer;
@@ -58,29 +67,59 @@ typedef struct _CalcSourceStream
 /// @brief Creates a new source stream using as a source to fill the buffer
 ///        a NUL-terminated string of characters.
 /// @param text The string of characters o wrap into the new source stream.
+/// @param encoding The encoding of the stream.
 /// @return A pointer to the new source stream.
-CALC_API CalcSourceStream_t *CALC_STDCALL calcCreateSourceStreamFromText(const char *const text);
+CALC_API CalcSourceStream_t *CALC_STDCALL calcCreateSourceStreamFromText(const char *const text, CalcSourceEncoding_t encoding);
 /// @brief Creates a new source stream filling the buffer only once loading
 ///        the full content of the specified file.
 /// @param path The path to the file to load.
+/// @param cleanupPath This flag specifies if the path must be deleted.
+/// @param encoding The encoding of the stream.
 /// @return A pointer to the new source stream.
-CALC_API CalcSourceStream_t *CALC_STDCALL calcCreateSourceStreamFromFile(const char *const path, bool_t cleanupPath);
+CALC_API CalcSourceStream_t *CALC_STDCALL calcCreateSourceStreamFromFile(const char *const path, bool_t cleanupPath, CalcSourceEncoding_t encoding);
 
 /// @brief Creates a new source stream filling the source buffer only once
 ///        loading the full content of specified stream.
 /// @param stream The file stream to load.
+/// @param encoding The encoding of the stream.
 /// @return A pointer to the new source stream.
-CALC_API CalcSourceStream_t *CALC_STDCALL calcCreateSourceStreamFromStream(FILE *const stream);
+CALC_API CalcSourceStream_t *CALC_STDCALL calcCreateSourceStreamFromStream(FILE *const stream, CalcSourceEncoding_t encoding);
 
 /// @brief Opens a new source stream from a file specified by tha path
 ///        parameter.
 /// @param path The path to the file to open.
+/// @param cleanupPath This flag specifies if the path must be deleted.
+/// @param encoding The encoding of the stream.
 /// @return A pointer to the new source stream.
-CALC_API CalcSourceStream_t *CALC_STDCALL calcOpenSourceStream(const char *const path, bool_t cleanupPath);
+CALC_API CalcSourceStream_t *CALC_STDCALL calcOpenSourceStream(const char *const path, bool_t cleanupPath, CalcSourceEncoding_t encoding);
 /// @brief Opens the source stream that uses stdin as source file. This
 ///        function is a special case of the calcOpenSourceStream function.
 /// @return A pointer to the new source stream.
 CALC_API CalcSourceStream_t *CALC_STDCALL calcOpenStandardSourceStream(void);
+
+/// @brief Peeks the next character of the stream and returns its value.
+/// @param sourceStream A pointer to the source stream from which peek the
+///                     character.
+/// @return The value of the read character.
+CALC_API int32_t CALC_STDCALL calcSourceStreamPeek(CalcSourceStream_t *const sourceStream);
+/// @brief Reads the next character of the stream and returns its value.
+/// @param sourceStream A pointer to the source stream from which read the
+///                     character.
+/// @return The value of the read character.
+CALC_API int32_t CALC_STDCALL calcSourceStreamRead(CalcSourceStream_t *const sourceStream);
+
+/// @brief Peeks the character next to the specified offset from the stream.
+/// @param sourceStream A pointer to the source stream from which peek the
+///                     character.
+/// @param offset The offset of the character to read.
+/// @return The value of the read character.
+CALC_API int32_t CALC_STDCALL calcSourceStreamPeekOffset(CalcSourceStream_t *const sourceStream, uint32_t offset);
+/// @brief Reads the character next to the specified offset from the stream.
+/// @param sourceStream A pointer to the source stream from which read the
+///                     character.
+/// @param offset The offset of the character to read.
+/// @return The value of the read character.
+CALC_API int32_t CALC_STDCALL calcSourceStreamReadOffset(CalcSourceStream_t *const sourceStream, uint32_t offset);
 
 /// @brief Closes an open source stream.
 /// @param sourceStream A pointer to the source stream to close.
--- lib/source/source_stream.c
+++ lib/source/source_stream.c
@@ -6,7 +6,7 @@ static inline CalcSourceStream_t *CALC_STDCALL calc_InitializeSourceStream(CalcS
     sourceStream->stream = NULL;
 
     sourceStream->isStdin = FALSE;
-    sourceStream->isText = FALSE;
+    sourceStream->isInitialized = FALSE;
     sourceStream->isOpen = FALSE;
     sourceStream->cleanup = FALSE;
 
@@ -19,7 +19,7 @@ static inline CalcSourceStream_t *CALC_STDCALL calc_InitializeSourceStream(CalcS
     return sourceStream;
 }
 
-static inline CalcSourceStream_t *CALC_STDCALL calc_CreateSourceStream(const char *const path, FILE *const stream, bool_t isStdin, bool_t isText, bool_t isOpen, bool_t cleanup, CalcSourceBuffer_t *const sourceBuffer)
+static inline CalcSourceStream_t *CALC_STDCALL calc_CreateSourceStream(const char *const path, FILE *const stream, bool_t isStdin, bool_t isInitialized, bool_t isOpen, bool_t cleanup, CalcSourceEncoding_t encoding, CalcSourceBuffer_t *const sourceBuffer)
 {
     CalcSourceStream_t *sourceStream = alloc(CalcSourceStream_t);
 
@@ -27,10 +27,11 @@ static inline CalcSourceStream_t *CALC_STDCALL calc_CreateSourceStream(const cha
     sourceStream->stream = stream;
 
     sourceStream->isStdin = isStdin;
-    sourceStream->isText = isText;
+    sourceStream->isInitialized = isInitialized;
     sourceStream->isOpen = isOpen;
     sourceStream->cleanup = cleanup;
 
+    sourceStream->encoding = encoding;
     sourceStream->buffer = sourceBuffer;
 
     calcResetSourceLocation(&sourceStream->streamLocation);
@@ -40,12 +41,12 @@ static inline CalcSourceStream_t *CALC_STDCALL calc_CreateSourceStream(const cha
     return sourceStream;
 }
 
-CALC_API CalcSourceStream_t *CALC_STDCALL calcCreateSourceStreamFromText(const char *const text)
+CALC_API CalcSourceStream_t *CALC_STDCALL calcCreateSourceStreamFromText(const char *const text, CalcSourceEncoding_t encoding)
 {
-    return calc_CreateSourceStream(NULL, NULL, FALSE, TRUE, FALSE, FALSE, calcCreateSourceBufferFromText(text));
+    return calc_CreateSourceStream(NULL, NULL, FALSE, FALSE, FALSE, FALSE, encoding, calcCreateSourceBufferFromText(text));
 }
 
-CALC_API CalcSourceStream_t *CALC_STDCALL calcCreateSourceStreamFromFile(const char *const path, bool_t cleanupPath)
+CALC_API CalcSourceStream_t *CALC_STDCALL calcCreateSourceStreamFromFile(const char *const path, bool_t cleanupPath, CalcSourceEncoding_t encoding)
 {
     assert(path != NULL);
 
@@ -54,10 +55,10 @@ CALC_API CalcSourceStream_t *CALC_STDCALL calcCreateSourceStreamFromFile(const c
     if (!sourceBuffer)
         return NULL;
     else
-        return calc_CreateSourceStream(path, NULL, FALSE, FALSE, FALSE, cleanupPath, sourceBuffer);
+        return calc_CreateSourceStream(path, NULL, FALSE, FALSE, FALSE, cleanupPath, encoding, sourceBuffer);
 }
 
-CALC_API CalcSourceStream_t *CALC_STDCALL calcCreateSourceStreamFromStream(FILE *const stream)
+CALC_API CalcSourceStream_t *CALC_STDCALL calcCreateSourceStreamFromStream(FILE *const stream, CalcSourceEncoding_t encoding)
 {
     assert(stream != NULL);
 
@@ -66,10 +67,10 @@ CALC_API CalcSourceStream_t *CALC_STDCALL calcCreateSourceStreamFromStream(FILE 
     if (!sourceBuffer)
         return NULL;
     else
-        return calc_CreateSourceStream(NULL, NULL, FALSE, FALSE, FALSE, FALSE, sourceBuffer);
+        return calc_CreateSourceStream(NULL, NULL, FALSE, FALSE, FALSE, FALSE, encoding, sourceBuffer);
 }
 
-CALC_API CalcSourceStream_t *CALC_STDCALL calcOpenSourceStream(const char *const path, bool_t cleanupPath)
+CALC_API CalcSourceStream_t *CALC_STDCALL calcOpenSourceStream(const char *const path, bool_t cleanupPath, CalcSourceEncoding_t encoding)
 {
     assert(path != NULL);
 
@@ -78,12 +79,106 @@ CALC_API CalcSourceStream_t *CALC_STDCALL calcOpenSourceStream(const char *const
     if (!stream)
         return NULL;
 
-    return calc_CreateSourceStream(path, stream, FALSE, FALSE, TRUE, cleanupPath, calcCreateSourceBuffer(CALC_PAGESIZ, NULL, 0));
+    return calc_CreateSourceStream(path, stream, FALSE, FALSE, TRUE, cleanupPath, encoding, calcCreateSourceBuffer(CALC_PAGESIZ, NULL, 0));
 }
 
 CALC_API CalcSourceStream_t *CALC_STDCALL calcOpenStandardSourceStream(void)
 {
-    return calc_CreateSourceStream("<stdin>", stdin, TRUE, FALSE, TRUE, FALSE, calcCreateSourceBuffer(CALC_PAGESIZ, NULL, 0));
+    return calc_CreateSourceStream("<stdin>", stdin, TRUE, FALSE, TRUE, FALSE, CALC_DEFAULT_ENCODING, calcCreateSourceBuffer(CALC_PAGESIZ, NULL, 0));
+}
+
+static inline bool_t CALC_STDCALL calc_SourceStreamNeedsARefill(CalcSourceStream_t *const sourceStream, uint32_t offset)
+{
+    return !sourceStream->isInitialized || ((sourceStream->forwardLocation.ch + offset) >= sourceStream->buffer->size);
+}
+
+static inline bool_t CALC_STDCALL calc_SourceStreamRefill(CalcSourceStream_t *const sourceStream)
+{
+    if (!sourceStream->isOpen || !sourceStream->stream)
+        return FALSE;
+
+    FILE *stream = sourceStream->stream;
+    byte_t *data = sourceStream->buffer->data;
+    size_t begin = sourceStream->beginLocation.ch, count = sourceStream->buffer->size - begin;
+
+    if (feof(stream))
+        return FALSE;
+
+    if (begin)
+        bufmov(data, (data + begin), count);
+
+    if (!sourceStream->isStdin)
+        fread((void *)(data + begin), sizeof(byte_t), count, stream);
+    else
+        fgets((char *)(data + begin), count, stream);
+
+    if (!sourceStream->isInitialized)
+        sourceStream->isInitialized = TRUE;
+
+    return TRUE;
+}
+
+CALC_API int32_t CALC_STDCALL calcSourceStreamPeek(CalcSourceStream_t *const sourceStream)
+{
+    return calcSourceBufferGetChar(sourceStream->buffer, sourceStream->encoding, sourceStream->forwardLocation.ch, NULL);
+}
+
+static inline int32_t CALC_STDCALL calc_SourceStreamRead(CalcSourceStream_t *const sourceStream, ssize_t *const outOffset)
+{
+    if (calc_SourceStreamNeedsARefill(sourceStream, 0) && !calc_SourceStreamRefill(sourceStream))
+        return EOF;
+
+    int32_t result;
+    ssize_t offset;
+
+    result = calcSourceBufferGetChar(sourceStream->buffer, sourceStream->encoding, sourceStream->forwardLocation.ch, &offset);
+
+    switch (result)
+    {
+    case EOF:
+    case NUL:
+        break;
+
+    case EOL:
+        sourceStream->forwardLocation.co = 0;
+        sourceStream->forwardLocation.ln++;
+        break;
+
+    default:
+        sourceStream->forwardLocation.co += offset;
+        break;
+    }
+
+    sourceStream->forwardLocation.ch += offset;
+
+    if (outOffset && *outOffset)
+        *outOffset = offset;
+
+    return result;
+}
+
+CALC_API int32_t CALC_STDCALL calcSourceStreamRead(CalcSourceStream_t *const sourceStream)
+{
+    return calc_SourceStreamRead(sourceStream, NULL);
+}
+
+CALC_API int32_t CALC_STDCALL calcSourceStreamPeekOffset(CalcSourceStream_t *const sourceStream, uint32_t offset)
+{
+    return calcSourceBufferGetChar(sourceStream->buffer, sourceStream->encoding, sourceStream->forwardLocation.ch + offset, NULL);
+}
+
+CALC_API int32_t CALC_STDCALL calcSourceStreamReadOffset(CalcSourceStream_t *const sourceStream, uint32_t offset)
+{
+    if (calc_SourceStreamNeedsARefill(sourceStream, offset) && !calc_SourceStreamRefill(sourceStream))
+        return EOF;
+
+    int32_t result;
+    uint32_t i;
+
+    for (i = 0; i < offset; i++)
+        result = calcSourceStreamRead(sourceStream);
+
+    return result;
 }
 
 CALC_API bool_t CALC_STDCALL calcCloseSourceStream(CalcSourceStream_t *const sourceStream)
