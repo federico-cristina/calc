5beb64a60c11119727bf5e79c675a43b8a5fd0ac
Federico Cristina <federico.cristina@outlook.it>
2024-09-29 11:14:15 +0200

    Update test_source_stream.c

--- units/source/test_source_stream.c
+++ units/source/test_source_stream.c
@@ -1,8 +1,23 @@
 #include "calc/source/source_stream.h"
 
+#include <time.h>
+
 int main()
 {
     CalcSourceStream_t *s = calcOpenSourceStream(CALC_CURRENT_PATH "/docs/examples/f.calc", FALSE, CALC_DEFAULT_ENCODING);
+
+    double T1, T2, dT;
+
+    T1 = (double)clock() / CLOCKS_PER_SEC;
+
+    while (calcSourceStreamPeek(s) != EOF)
+        printf("%c", calcSourceStreamRead(s));
+
+    T2 = (double)clock() / CLOCKS_PER_SEC;
+
+    dT = T2 - T1;
+
+    printf("\nTime: %g ms\n", dT);
 
     calcDeleteSourceStream(s);
 }
