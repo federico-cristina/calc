daf6674593a7987ac85ed8491c84fc62b0981207
federico-cristina <federico.cristina@outlook.it>
2024-09-30 17:14:09 +0200

    Update dload.h (add header comment)

--- include/calc/base/dload.h
+++ include/calc/base/dload.h
@@ -1,7 +1,18 @@
 #pragma once
 
 /**
+ * @file        dload.h
  *
+ * @author      Federico Cristina <federico.cristina@outlook.it>
+ *
+ * @copyright   Copyright (c) 2024 Federico Cristina
+ *
+ *              This file is part of the calc scripting language project,
+ *              under the Apache License v2.0. See LICENSE for license
+ *              informations.
+ *
+ * @brief       In this header are defined functions to load dynamically
+ *              symbols and shared modules.
  */
 
 #ifndef CALC_BASE_DLOAD_H_
