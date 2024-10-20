1d0b0c1aa63c743bd0704ad0e2a4b05a60c5b8d8
Federico Cristina <federico.cristina@outlook.it>
2024-10-20 11:52:57 +0200

    Improve utils.h

--- include/calc/base/utils.h
+++ include/calc/base/utils.h
@@ -7,50 +7,60 @@
  *
  * @copyright   Copyright (c) 2024 Federico Cristina
  *
- *              This file is part of the calc scripting language project,
- *              under the Apache License v2.0. See LICENSE for license
+ *              This file is part of the calc programming language project,
+ *              under the Apache License v2.0. See LICENSE file for license
  *              informations.
  *
+ * @version     1.1
+ *
  * @brief       In this header are defined utility macros to compare two
- *              numeric values, find the minimum value and find the maximum
- *              value.
+ *              numeric values, find the minimum or the maximum value between
+ *              thwo numbers and check if an integer has a bitflag.
  */
 
 #ifndef CALC_BASE_UTILS_H_
 #define CALC_BASE_UTILS_H_
 
 #ifndef numcmp
-/// @brief Compares two numbers.
-/// @param x First number to compare.
-/// @param y Second number to compare.
-/// @return -1 if x is less than y, +1 if x is grater than y, or
-///         0 if x and y have the same value.
+/**
+ * @brief       Compares two numbers.
+ * @param x     First number to compare.
+ * @param y     Second number to compare.
+ * @return      -1 if x is less than y, +1 if x is grater than y, or 0 if x and
+ *              y have the same value.
+ */
 #   define numcmp(x, y) ((x) < (y) ? -1 : \
                          (x) > (y) ? +1 : 0)
-#endif // numcmp
+#endif
 
 #ifndef min
-/// @brief Gets the minimum value between its two parameters.
-/// @param size1 First size.
-/// @param size2 Second size.
-/// @return size1 if is less than size2, else size2.
-#   define min(size1, size2) (((size1) < (size2)) ? (size1) : (size2))
-#endif // min
+/**
+ * @brief       Gets the minimum value between its two parameters.
+ * @param x     First number.
+ * @param y     Second number.
+ * @return      x if is less than y, else y.
+ */
+#   define min(x, y) (((x) < (y)) ? (x) : (y))
+#endif
 
 #ifndef max
-/// @brief Gets the maximum value between its two parameters.
-/// @param size1 First size.
-/// @param size2 Second size.
-/// @return size1 if is greater than size2, else size2.
-#   define max(size1, size2) (((size1) > (size2)) ? (size1) : (size2))
-#endif // max
+/**
+ * @brief       Gets the maximum value between its two parameters.
+ * @param x     First number.
+ * @param y     Second number.
+ * @return      x if is greater than y, else y.
+ */
+#   define max(x, y) (((x) > (y)) ? (x) : (y))
+#endif
 
 #ifndef hasflag
-/// @brief Checks if an integral value has a bit flag.
-/// @param value Value to check.
-/// @param flag Bit flag to check.
-/// @return 1 if value has the bit flag, else 0.
+/**
+ * @brief       Checks if an integral value has a bit flag.
+ * @param value Value to check.
+ * @param flag  Bit flag to check.
+ * @return      1 if value has the bit flag, else 0.
+ */
 #   define hasflag(value, flag) (((value) & (flag)) == (flag))
-#endif // hasflag
+#endif
 
-#endif // CALC_BASE_UTILS_H_
+#endif /* CALC_BASE_UTILS_H_ */
