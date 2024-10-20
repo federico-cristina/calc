70f5bf05d81c8d429884bcd04d2ce104528afac7
Federico Cristina <federico.cristina@outlook.it>
2024-10-20 11:37:09 +0200

    Improve api.h

--- include/calc/base/api.h
+++ include/calc/base/api.h
@@ -1,18 +1,20 @@
 #pragma once
 
 /**
- * @file        api.h
+ * @file        config.h
  *
  * @author      Federico Cristina <federico.cristina@outlook.it>
  *
  * @copyright   Copyright (c) 2024 Federico Cristina
  *
- *              This file is part of the calc scripting language project,
- *              under the Apache License v2.0. See LICENSE for license
+ *              This file is part of the calc programming language project,
+ *              under the Apache License v2.0. See LICENSE file for license
  *              informations.
  *
- * @brief       In this header are defined utility macro to functions
- *              exportations while building shared libraries.
+ * @version     1.1
+ *
+ * @brief       In this header are defined utility macro for API constructions
+ *              and use.
  */
 
 #ifndef CALC_BASE_API_H_
@@ -20,49 +22,60 @@
 
 #include "calc/base/defs.h"
 
-#ifdef CALC_NO_DLL
-#   ifndef CALC_API_PUB
-/// @brief This macro marks a function to be exported
-///        while building a dynamic library.
-#       define CALC_API_PUB
-#   endif // CALC_API_PUB
-#endif
-
-#ifndef CALC_API_PUB
-#   if CALC_BUILD_AS_DLL
-/// @brief This macro marks a function to be exported
-///        while building a dynamic library.
-#       define CALC_API_PUB CALC_DLL_EXPORT
+#ifndef CALC_API_PUBLIC
+#   if !defined CALC_NO_DLL && !defined _NO_DLL
+#       if CALC_BUILD_AS_DLL
+/**
+ * @brief       This constant marks a function as visible at the extern
+ *              of the API in which is declared.
+ */
+#           define CALC_API_PUBLIC CALC_DLL_EXPORT
+#       else
+/**
+ * @brief       This constant marks a function as visible at the extern
+ *              of the API in which is declared.
+ */
+#           define CALC_API_PUBLIC CALC_DLL_IMPORT
+#       endif
 #   else
-/// @brief This macro marks a function to be imported
-///        while building a dynamic library.
-#       define CALC_API_PUB CALC_DLL_IMPORT
-#   endif // CALC_API_PUB
-#endif // CALC_API
-
-#ifndef CALC_API_STATIC
-/// @brief This macro marks a function to be hidden
-///        while building a dynamic library.
-#   define CALC_API_STATIC CALC_DLL_STATIC
-#endif // CALC_API_STATIC
-
-#ifndef CALC_API
-/// @brief This macro marks a function as an API
-///        function. By default API functions are
-///        public.
-#   define CALC_API CALC_API_PUB
-#endif // CALC_API
+/**
+ * @brief       This constant marks a function as visible at the extern
+ *              of the API in which is declared.
+ */
+#       define CALC_API_PUBLIC
+#   endif
+#endif
 
 #ifndef CALC_API_INLINE
 #   if CALC_COMPILER_HAS_DECLSPEC
-/// @brief This macro marks a function as an API
-///        function that can be inlined.
-#       define CALC_API_INLINE CALC_API_PUB CALC_INLINE
+/**
+ * @brief       This constant marks an inline function as visible at the
+ *              extern of the API in which is declared, may be not supported.
+ */
+#       define CALC_API_INLINE CALC_API_PUBLIC CALC_INLINE
 #   else
-/// @brief This macro marks a function as an API
-///        function that can be inlined.
+/**
+ * @brief       This constant marks an inline function as visible at the
+ *              extern of the API in which is declared, may be not supported.
+ */
 #       define CALC_API_INLINE CALC_INLINE
 #   endif
-#endif // CALC_API_INLINE
+#endif
 
-#endif // CALC_BASE_API_H_
+#ifndef CALC_API_STATIC
+/**
+ * @brief       This constant marks a function as visible only in the
+ *              scope of the API in which is declared.
+ */
+#   define CALC_API_STATIC CALC_DLL_STATIC
+#endif
+
+#ifndef CALC_API
+/**
+ * @brief       This constant marks a function as part of the current
+ *              API.
+ */
+#   define CALC_API CALC_API_PUBLIC
+#endif
+
+#endif /* CALC_BASE_API_H_ */
