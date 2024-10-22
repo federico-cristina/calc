697c502ec912205605ad452dde7132e5ee06cc07
Federico Cristina <federico.cristina@outlook.it>
2024-10-22 17:38:22 +0200

    Improve dload.h

--- include/calc/base/dload.h
+++ include/calc/base/dload.h
@@ -7,9 +7,11 @@
  *
  * @copyright   Copyright (c) 2024 Federico Cristina
  *
- *              This file is part of the calc scripting language project,
- *              under the Apache License v2.0. See LICENSE for license
+ *              This file is part of the calc programming language project,
+ *              under the Apache License v2.0. See LICENSE file for license
  *              informations.
+ *
+ * @version     1.1
  *
  * @brief       In this header are defined functions to load dynamically
  *              symbols and shared modules.
@@ -22,31 +24,44 @@
 
 CALC_C_HEADER_BEGIN
 
-/// @brief Handle datatype.
+/**
+ * @brief       Handle datatype.
+ */
 typedef void *handle_t;
 
-/// @brief Loads the handle for the current module.
-/// @return An opaque handle to the current module.
+/**
+ * @brief       Loads the handle for the current module.
+ * 
+ * @return      An opaque handle to the current module. 
+ */
 CALC_EXTERN handle_t CALC_STDCALL dlload_current(void);
-/// @brief Loads the specified dynamic (shared) library.
-/// @param path The path to or the name of the dynamic
-///             (shared) library to load.
-/// @return An opaque handle to the specified module.
+/**
+ * @brief       Loads the specified dynamic (shared) library.
+ * 
+ * @param       path The path to or the name of the dynamic library to load.
+ * @return      An opaque handle to the specified module. 
+ */
 CALC_EXTERN handle_t CALC_STDCALL dlload(const char *const path);
 
-/// @brief Imports a function from the module pointed
-///        by the specified handle by its name.
-/// @param handle The handle from which load the function.
-/// @param name The name of the function to import.
-/// @return An opaque handle to the imported function.
+/**
+ * @brief       Imports a function from the module pointed by the specified
+ *              handle by its name.
+ * 
+ * @param       handle The handle from which load the function.
+ * @param       name The name of the function to import.
+ * @return      An opaque handle to the imported function. 
+ */
 CALC_EXTERN handle_t CALC_STDCALL dlimpf(handle_t handle, const char *const name);
 
-/// @brief Unloads the dynamic (shared) library pointed
-///        by the handle from the memory.
-/// @param handle The handle of the module to unload.
-/// @return TRUE in case of success, else FALSE.
+/**
+ * @brief       Unloads the dynamic (shared) library pointed by the handle
+ *              from the memory.
+ * 
+ * @param       handle The handle of the module to unload.
+ * @return      TRUE in case of success, else FALSE. 
+ */
 CALC_EXTERN bool_t CALC_STDCALL dlunload(handle_t handle);
 
 CALC_C_HEADER_END
 
-#endif // CALC_BASE_DLOAD_H_
+#endif /* CALC_BASE_DLOAD_H_ */
